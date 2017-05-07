706d8900b4294c54622d40a8cc3b6b53bddfbca2
MyStar219 <star1985219@yahoo.com>
2017-05-07 08:49:50 +0800

    Latest Version

---
+++ Kanotguiden-2/Kanotguiden-2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Kanotguiden-2
+//
+//  Created by Prince on 5/2/17.
+//  Copyright © 2017 developteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
